6e93924fa0c11f15a003fc93f849762a5e4ba859
NicolasRadic <nicolas.radic05@gmail.com>
2023-03-15 17:09:29 +0100

    add got to top button in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { ChevronDoubleUpIcon } from "@heroicons/react/24/solid";
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className="bg-[rgb(197,157,95)] text-white">
+    <div className="bg-[rgb(197,157,95)] text-white relative">
       <div className="py-10 flex flex-col items-center md:flex-row justify-center gap-8">
         <div className="flex gap-6">
           <a
@@ -43,6 +51,13 @@ const Footer = () => {
         <p>Chaussée de Hannut 364, Bierset</p>
         <p>contact@LCDHC.be</p>
         <p>04 250 21 13</p>
+      </div>
+      <div className="absolute z-10 bottom-10 right-5 md:bottom-3 lg:bottom-10 lg:right-10">
+        {" "}
+        <ChevronDoubleUpIcon
+          onClick={() => scrollToTop()}
+          className="w-8 pt-12"
+        />
       </div>
     </div>
   );
