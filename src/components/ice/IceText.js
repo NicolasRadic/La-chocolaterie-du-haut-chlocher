3469d7ce6ab716610c1630cb862d40bf925d95db
NicolasRadic <nicolas.radic05@gmail.com>
2023-09-04 15:14:23 +0200

    change style image

--- src/components/ice/IceText.js
+++ src/components/ice/IceText.js
@@ -36,7 +36,7 @@ const IceText = () => {
             demi-litres.
           </a>
         </p>
-        <div className="pt-10 flex flex-col w-full justify-center gap-4  sm:flex-row">
+        <div className="pt-10 flex flex-col w-full justify-between  sm:flex-row">
           <img
             src="./images/ice/gateau_glace-1.jpg"
             alt="chocolat équipe auto pneu"
